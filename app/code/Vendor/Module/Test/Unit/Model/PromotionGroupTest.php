b498ee7bf602f2d9cd76faa393a04fdf80608e3a
opstrzemieczny <153062189+opstrzemieczny@users.noreply.github.com>
2023-12-06 08:06:33 +0100

    Create PromotionGroupTest.php

---
+++ app/code/Vendor/Module/Test/Unit/Model/PromotionGroupTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Vendor\Module\Test\Unit\Model;
+
+use PHPUnit\Framework\TestCase;
+use Vendor\Module\Model\PromotionGroup;
+
+class PromotionGroupTest extends TestCase
+{
+    // Testuj metody w modelu PromotionGroup
+}
