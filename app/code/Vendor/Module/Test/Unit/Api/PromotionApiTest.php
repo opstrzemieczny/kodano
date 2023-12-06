1efa2ebaa92d58ff3c01d8e813d18a745e09a03e
opstrzemieczny <153062189+opstrzemieczny@users.noreply.github.com>
2023-12-06 08:12:07 +0100

    Update PromotionApiTest.php

--- app/code/Vendor/Module/Test/Unit/Api/PromotionApiTest.php
+++ app/code/Vendor/Module/Test/Unit/Api/PromotionApiTest.php
@@ -4,8 +4,45 @@ namespace Vendor\Module\Test\Unit\Api;
 
 use PHPUnit\Framework\TestCase;
 use Vendor\Module\Api\PromotionApi;
+use Vendor\Module\Model\Promotion;
 
 class PromotionApiTest extends TestCase
 {
-    // Testuj metody w kontrolerze API
+    protected $objectManager;
+    protected $promotionModelMock;
+
+    protected function setUp(): void
+    {
+        $this->objectManager = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+
+        $this->promotionModelMock = $this->getMockBuilder(Promotion::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function testAddPromotion()
+    {
+        $data = [
+            'name' => 'Test Promotion',
+            // Add other required fields
+        ];
+
+        $promotionApi = $this->objectManager->getObject(
+            PromotionApi::class,
+            ['promotionModel' => $this->promotionModelMock]
+        );
+
+        $this->promotionModelMock->expects($this->once())
+            ->method('setData')
+            ->with($this->equalTo($data));
+
+        $this->promotionModelMock->expects($this->once())
+            ->method('save');
+
+        $result = $promotionApi->addPromotion($data);
+        $this->assertTrue($result['success']);
+        $this->assertEquals('Promotion added successfully.', $result['message']);
+    }
+
+    // Add similar tests for other methods
 }
