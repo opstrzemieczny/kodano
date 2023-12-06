bdf56463e929813d432d13afdab11db1ddb5627a
opstrzemieczny <153062189+opstrzemieczny@users.noreply.github.com>
2023-12-06 08:16:32 +0100

    Update PromotionGroup.php

--- app/code/Vendor/Module/Model/PromotionGroup.php
+++ app/code/Vendor/Module/Model/PromotionGroup.php
@@ -3,6 +3,7 @@
 namespace Vendor\Module\Model;
 
 use Magento\Framework\Model\AbstractModel;
+use Vendor\Module\Model\ResourceModel\PromotionGroup as PromotionGroupResource;
 
 class PromotionGroup extends AbstractModel
 {
@@ -10,8 +11,52 @@ class PromotionGroup extends AbstractModel
     protected $_cacheTag = 'promotion_groups';
     protected $_eventPrefix = 'promotion_groups';
 
+    protected $promotionGroupResource;
+
+    public function __construct(
+        PromotionGroupResource $promotionGroupResource,
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        array $data = []
+    ) {
+        parent::__construct($context, $registry, null, null, $data);
+        $this->promotionGroupResource = $promotionGroupResource;
+    }
+
     protected function _construct()
     {
-        $this->_init(ResourceModel\PromotionGroup::class);
+        $this->_init(PromotionGroupResource::class);
     }
+
+    public function getName()
+    {
+        return $this->_getData('name');
+    }
+
+    public function setName($name)
+    {
+        return $this->setData('name', $name);
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->_getData('created_at');
+    }
+
+    public function setCreatedAt($createdAt)
+    {
+        return $this->setData('created_at', $createdAt);
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->_getData('updated_at');
+    }
+
+    public function setUpdatedAt($updatedAt)
+    {
+        return $this->setData('updated_at', $updatedAt);
+    }
+    
+    // Dodaj inne metody i funkcje związane z modelem, jeśli są potrzebne
 }
