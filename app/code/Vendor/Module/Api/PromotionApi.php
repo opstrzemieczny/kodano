d4e1a93e7e401cc636a8755d5b6bf77a9855ae60
opstrzemieczny <153062189+opstrzemieczny@users.noreply.github.com>
2023-12-06 08:10:52 +0100

    Update PromotionApi.php

--- app/code/Vendor/Module/Api/PromotionApi.php
+++ app/code/Vendor/Module/Api/PromotionApi.php
@@ -3,15 +3,19 @@
 namespace Vendor\Module\Api;
 
 use Vendor\Module\Model\Promotion;
+use Vendor\Module\Model\PromotionGroup;
 
 class PromotionApi implements PromotionApiInterface
 {
     protected $promotionModel;
+    protected $promotionGroupModel;
 
     public function __construct(
-        Promotion $promotionModel
+        Promotion $promotionModel,
+        PromotionGroup $promotionGroupModel
     ) {
         $this->promotionModel = $promotionModel;
+        $this->promotionGroupModel = $promotionGroupModel;
     }
 
     public function getList()
@@ -26,11 +30,28 @@ class PromotionApi implements PromotionApiInterface
 
     public function addPromotion(array $data)
     {
-        // Implementacja dodawania promocji
+        try {
+            $promotion = $this->promotionModel;
+            $promotion->setData($data);
+            $promotion->save();
+            return ['success' => true, 'message' => 'Promotion added successfully.'];
+        } catch (\Exception $e) {
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
     }
 
     public function deletePromotion($id)
     {
-        // Implementacja usuwania promocji
+        try {
+            $promotion = $this->promotionModel->load($id);
+            if ($promotion->getId()) {
+                $promotion->delete();
+                return ['success' => true, 'message' => 'Promotion deleted successfully.'];
+            } else {
+                return ['success' => false, 'message' => 'Promotion not found.'];
+            }
+        } catch (\Exception $e) {
+            return ['success' => false, 'message' => $e->getMessage()];
+        }
     }
 }
