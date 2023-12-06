a512ad7de5e8c7f57de815a94ef606c012343f31
opstrzemieczny <153062189+opstrzemieczny@users.noreply.github.com>
2023-12-06 08:07:28 +0100

    Create PromotionApiTest.php

---
+++ app/code/Vendor/Module/Test/Unit/Api/PromotionApiTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Vendor\Module\Test\Unit\Api;
+
+use PHPUnit\Framework\TestCase;
+use Vendor\Module\Api\PromotionApi;
+
+class PromotionApiTest extends TestCase
+{
+    // Testuj metody w kontrolerze API
+}
