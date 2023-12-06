439a5198517500acdd25a6d37b1b8b4cb670c211
opstrzemieczny <153062189+opstrzemieczny@users.noreply.github.com>
2023-12-06 08:00:05 +0100

    Create InstallSchema.php

---
+++ app/code/Vendor/Module/Setup/InstallSchema.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Vendor\Module\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $installer->startSetup();
+
+        $table = $installer->getConnection()
+            ->newTable($installer->getTable('promotions'))
+            ->addColumn(
+                'promotion_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+                'Promotion ID'
+            )
+            ->addColumn(
+                'name',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                ['nullable' => false],
+                'Promotion Name'
+            )
+            ->addColumn(
+                'created_at',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                null,
+                ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+                'Created At'
+            )
+            ->addColumn(
+                'updated_at',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                null,
+                ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
+                'Updated At'
+            )
+            ->setComment('Promotions Table');
+
+        $installer->getConnection()->createTable($table);
+
+        $table = $installer->getConnection()
+            ->newTable($installer->getTable('promotion_groups'))
+            // Dodaj definicję tabeli dla promotion_groups
+            ->setComment('Promotion Groups Table');
+
+        $installer->getConnection()->createTable($table);
+
+        $installer->endSetup();
+    }
+}
